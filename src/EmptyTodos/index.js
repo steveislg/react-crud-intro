8ae2970902024321a0abb4ca148d44d23d3cade9
steveislg <ilazo@invebchile.cl>
2024-07-09 17:40:06 -0400

    Cambio des Descripcion de TODO por Tarea

--- src/EmptyTodos/index.js
+++ src/EmptyTodos/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function EmptyTodos() {
   return (
-    <p>¡Crea tu primer TODO!</p>
+    <p>¡Crea tu primera Tarea!</p>
   );
-}
+} 
 
 export { EmptyTodos };
--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -10,7 +10,7 @@ function TodoCounter() {
   
   return (
     <h1 className="TodoCounter">
-      Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span> TODOs
+      Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span> Tareas
     </h1>
   );
 }
